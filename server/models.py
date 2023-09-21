232853ddc84a3d1acb867d8bdf92efd74c64ba21
KIPROTYCH <kiprotych@gmail.com>
2023-09-22 00:16:33 +0300

    Completed Assignment.

--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -7,3 +8,6 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    price = db.Column(db.Float)
